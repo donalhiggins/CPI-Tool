7b350b303e351318ca5492fed21b18d74d94bb6c
donalhiggins <dvh121103@gmail.com>
2021-09-01 15:25:22 -0500

    Add Question System

--- QuestionGUI.py
+++ QuestionGUI.py
@@ -1,14 +1,19 @@
 import tkinter as tk
+import tkinter
 from idlelib.tooltip import Hovertip
 import threading
-
+from tkinter import simpledialog
 
 class QuestionGUI(threading.Thread):
     flags = []
-    question = "temp_question"
-    info = "temp_info"
+    question = 'temp_question'
+    info = 'temp_info'
     questionText = None
     isChange = False
+    maptype = 'temp'
+    answer = False
+    answered = False
+    critical = []
 
     def __init__(self):
         t1 = threading.Thread(target=self.createGUI)
@@ -18,11 +23,11 @@ class QuestionGUI(threading.Thread):
     def createGUI(self):
         #CREATE root
         self.root = tk.Tk()
-        self.root.protocol("WM_DELETE_WINDOW", self.callback)
+        self.root.protocol('WM_DELETE_WINDOW', self.callback)
         self.logo = tk.PhotoImage(file='trigon.png')
         self.root.iconphoto(False, self.logo)
-        self.root.title("!!!TEMP!!!")
-        self.root.geometry("1280x720")
+        self.root.title('CPI Tool')
+        self.root.geometry('1280x720')
 
         #CREATE INFO BUTTON
         self.infobtn = tk.PhotoImage(file='info.png')
@@ -31,23 +36,23 @@ class QuestionGUI(threading.Thread):
 
         #CREATE FLAG BUTTON
         self.flagbtn = tk.PhotoImage(file='flag.png')
-        self.flagbutton = tk.Button(self.root, image=self.flagbtn, command=self.flag())
+        self.flagbutton = tk.Button(self.root, image=self.flagbtn, command=self.flag)
 
         #CREATE YES/NO BUTTONS
-        self.yesbutton = tk.Button(self.root, text='Yes', font=('Times New Roman', 14), command=self.yesButton())
-        self.nobutton = tk.Button(self.root, text='No', font=('Times New Roman', 14), command=self.noButton())
+        self.yesbutton = tk.Button(self.root, text='Yes', font=('Times New Roman', 14), command=self.yesButton)
+        self.nobutton = tk.Button(self.root, text='No', font=('Times New Roman', 14), command=self.noButton)
 
         #CREATE TITLE AND QUESTION
-        self.header = tk.Label(self.root, text='!!!TEMP!!!', font=('Times New Roman', 24, 'bold'))
-        self.questionText = tk.Label(self.root, text='aaaaa', font=('Times New Roman', 14))
+        self.mapheader = tk.Label(self.root, text=self.maptype, font=('Times New Roman', 24, 'bold'))
+        self.questionText = tk.Label(self.root, text='', font=('Times New Roman', 14))
 
         #ARRANGE ELEMENTS
-        self.header.grid(row=0, column=0)
-        self.questionText.grid(row=8, column=5)
-        self.tipbutton.grid(row=8, column=10)
-        self.yesbutton.grid(row=25, column=15)
-        self.nobutton.grid(row=25, column=35)
-        self.flagbutton.grid(row=30, column=40)
+        self.mapheader.place(x=0, y=0)
+        self.questionText.place(x=10, y=40)
+        self.tipbutton.place(x=100, y=40)
+        self.yesbutton.place(x=40, y=80)
+        self.nobutton.place(x=85, y=80)
+        self.flagbutton.place(x=125, y=80)
 
         #UPDATE GUI
         self.updateGUI()
@@ -57,6 +62,7 @@ class QuestionGUI(threading.Thread):
     def updateGUI(self):
         if self.isChange:
             self.questionText.config(text=self.question)
+            self.mapheader.config(text=self.maptype)
             self.tip.text = self.info
 
         self.root.after(1000, self.updateGUI)
@@ -65,15 +71,27 @@ class QuestionGUI(threading.Thread):
         self.root.quit()
 
     def flag(self):
+        self.flags.append(simpledialog.askstring(title='Flag', prompt='Why?'))
         return None
-
+        
     def yesButton(self):
-        return True
+        self.answer = True
+        self.answered = True
+        self.getAnswer()
 
     def noButton(self):
-        return False
+        self.answer = False
+        self.answered = True
+        self.getAnswer()
 
-    def updateQuestion(self, quest, inf):
+    def getAnswer(self):
+        return self.answer
+
+    def updateQuestion(self, quest, inf, mapt=maptype):
         self.isChange = True
         self.question = quest
         self.info = inf
+        self.maptype = mapt
+
+    def addCritical(self, questnum):
+        self.critical.append(questnum)
--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 from QuestionGUI import QuestionGUI
 
 def main():
-    a = QuestionGUI()
-    a.updateQuestion('test', 'test')
+    maps = ['1. Concepts Map', '2. Materials Map',
+     '3. Design of Components/Subsystems/Systems Map', '4. Manufacturing Process Map',
+      '5. Integration of Components/Subsytems/Systems Map', '6. Operational Interface Process Map',
+       '7. Operational Concept Map']
+    questions = {maps[0] : [['Is the concept in the public domain', [2, 1]], ['Are other countries/organziations pursuing?', [3, 4]],
+     ['Does the concept provide us an Enhanced Capability?', ['Ok', 'Critical']], ['Would divulging it cause Public Outcry or Diplomatic Harm?', ['Ok', 'Critical']],
+     ['Has a demostrator been developed by another country/organization?', [5, 6]], ['Have we developed a demonstrator?', ['Ok', 'Critical']],
+     ['Is our conceptual approach markedly different?', ['Ok', 'Critical']]]}
+    
+    window = QuestionGUI()
+    output = 0
+    tempquestion = ''
+    window.updateQuestion('test', 'test', maps[0])
+
+    while(isinstance(output, int)):
+        window.updateQuestion(questions[maps[0]][output][0], 'temp')
+
+        if(window.answer and window.answered):
+            if(not isinstance(questions[maps[0]][output][1][1], int)):
+                tempquestion = questions[maps[0]][output][1][1]
+            output = questions[maps[0]][output][1][1]
+            window.answered = False
+        elif(not window.answer and window.answered):
+            if(not isinstance(questions[maps[0]][output][1][0], int)):
+                tempquestion = questions[maps[0]][output][1][0]
+            output = questions[maps[0]][output][1][0]
+            window.answered = False
+
+    window.addCritical(tempquestion)
+    print(window.critical)
+    
+
+
     
 
 if __name__ == "__main__":
