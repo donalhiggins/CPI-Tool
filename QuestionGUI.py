e68d02e23bc73c9377d73eee117b2a0c2a2ec828
donalhiggins <74075366+donalhiggins@users.noreply.github.com>
2021-09-08 23:52:54 -0500

    Added new things
    Added a start page, ability to skip questions, ability to name a file that the info is saved to, created a way to save the information about criticals and flags, and added an end screen.

--- QuestionGUI.py
+++ QuestionGUI.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter.constants import LEFT, RIGHT, TOP
 from idlelib.tooltip import Hovertip
 from ttkthemes import ThemedTk
 import json
+from PIL import ImageTk, Image
 
 class QuestionGUI():
     flags = []
@@ -16,6 +18,10 @@ class QuestionGUI():
     maps = []
     output = 0
     mapnum = 0
+    skippedQuestions = []
+    isSkip = False
+    endString = ''
+    testName = ''
 
     def __init__(self):
         # READS IN DICTIONARY AND LIST FOR QUESTIONS
@@ -26,17 +32,40 @@ class QuestionGUI():
         self.questions = json.loads(self.questions)
         self.maptype = self.maps[self.mapnum]
         self.question = self.questions[self.maptype][0][0]
-
-        self.createGUI()
-
-    def createGUI(self):
-        # CREATE root
+        
+        # CREATES root AND WELCOME WIDGETS
         self.root = ThemedTk(theme='arc')
+        self.name = tk.StringVar()
+        self.welcome = ttk.Label(self.root, text='Trigon Cyber CPI Tool', font=('Times New Roman', 30, 'bold'))
+        self.startbutton = ttk.Button(self.root, text='Start', command=self.createGUI)
+        self.enterName = ttk.Label(self.root, text='Enter the name of the utility you are testing.', font=('Times New Roman', 18))
+        self.nameBox = ttk.Entry(self.root, textvariable=self.name)
+        self.bigPhoto = ImageTk.PhotoImage(Image.open('bigtrigon.jpeg'))
+        self.biglogo = ttk.Label(self.root, image=self.bigPhoto)
+        self.startPage()
+    
+    def startPage(self):
+        # DECORATES root WITH WELCOME WIDGETS
         self.root['background'] = '#f5f6f7'
         self.logo = tk.PhotoImage(file='trigon.png')
         self.root.iconphoto(True, self.logo)
         self.root.title('CPI Tool')
         self.root.geometry('1280x720')
+        self.welcome.grid(row=0,column=0)
+        self.enterName.grid(row=1,column=0)
+        self.nameBox.grid(row=2,column=0)
+        self.startbutton.grid(row=3,column=0)
+        self.biglogo.grid(row=4,column=0)
+        self.root.mainloop()
+
+    def createGUI(self):
+        # REMOVE START SCREEN
+        self.testName = self.name.get()
+        self.welcome.destroy()
+        self.startbutton.destroy()
+        self.biglogo.destroy()
+        self.enterName.destroy()
+        self.nameBox.destroy()
 
         # CREATE INFO BUTTON
         self.infobtn = tk.PhotoImage(file='info.png')
@@ -73,59 +102,101 @@ class QuestionGUI():
         # UPDATES GUI
         self.updateGUI()
 
-        self.root.mainloop()
-
     def updateGUI(self):
-        # CHECKS IF A CHANGE IS NEEDED AND THEN EDITS GUI
-        if self.isChange:
-            self.questionText.config(text=self.question)
-            self.mapheader.config(text=self.maptype)
-            self.tip.text = self.info    
-            # IF HITS CRITICAL / OK GOES TO NEXT MAP   
-            if not isinstance(self.output, int):
-                if self.mapnum == 14:
-                    print(self.critical)
-                self.addCritical()
-                self.mapnum += 1
-                self.maptype = self.maps[self.mapnum]
-                self.output = 0
-                self.question = self.questions[self.maptype][0][0]
-            else:
-                self.critTrack = self.question
+        try:
+            # CHECKS IF A CHANGE IS NEEDED AND THEN EDITS GUI
+            if self.isChange:
+                self.questionText.config(text=self.question)
+                self.mapheader.config(text=self.maptype)
+                self.tip.text = self.info    
+                # IF HITS CRITICAL / OK GOES TO NEXT MAP AND SKIPS IF NEEDED
+                if not isinstance(self.output, int) or self.isSkip:
+                    if self.mapnum == 14:
+                        if len(self.skippedQuestions) == 0:
+                            self.endBox()
+                        else:
+                            self.endBox()
+                    else:
+                        self.addCritical()
+                        self.mapnum += 1
+                        self.maptype = self.maps[self.mapnum]
+                        self.output = 0
+                        self.question = self.questions[self.maptype][0][0]
+                        self.isSkip = False
+                else:
+                    self.critTrack = self.question
+        except tk.TclError:
+            self.file = open(self.testName, 'w')
+            self.file.write(self.endString)
 
         self.root.after(100, self.updateGUI)
 
     def flagWindow(self):
-        self.window = tk.Toplevel(self.root)
+        self.window = tk.Toplevel(self.root, bg='#f5f6f7')
         self.entry = tk.StringVar()
         self.window.title('Flag')
         ttk.Label(self.window, text='Why did you flag this?',
                   font=('Times New Roman', 14)).pack()
-        ttk.Entry(self.window, textvariable=self.entry).pack()
-        ttk.Button(self.window, text='Ok', command=self.closeFlagWindow).pack()
+        ttk.Entry(self.window, textvariable=self.entry).pack(side=TOP)
+        ttk.Button(self.window, text='Skip', command=self.skip).pack(side=RIGHT)
+        ttk.Button(self.window, text='Ok', command=self.closeFlagWindow).pack(side=LEFT)
 
     def closeFlagWindow(self):
-        self.flags.append(self.entry.get())
+        self.flags.append([self.question, self.entry.get()])
         self.window.destroy()
+
+    def critWindow(self):
+        self.window = tk.Toplevel(self.root, bg='#f5f6f7')
+        self.entry = tk.StringVar()
+        self.window.title('Critical')
+        ttk.Label(self.window, text=('Why was this critical? Question: ' + self.critTrack),
+                  font=('Times New Roman', 14)).pack()
+        ttk.Entry(self.window, textvariable=self.entry).pack()
+        ttk.Button(self.window, text='Ok', command=self.closeCritWindow).pack()
+        self.root.wait_window(self.window)
+
+    def skip(self):
+        self.flags.append([self.question, self.entry.get()])
+        self.skippedQuestions.append([self.mapnum, self.output])
+        self.isChange = True
+        self.isSkip = True
+        self.window.destroy()
+
+    def closeCritWindow(self):
+        self.critical.append([self.critTrack, self.entry.get()])
+        self.window.destroy()
+
+    def addCritical(self):
+        if self.output == 'Critical':
+            self.critWindow()
 
     def yesButton(self):
         self.isChange = True
-        self.output = self.questions[self.maptype][self.output][1][1]
+        if isinstance(self.output, int):
+            self.output = self.questions[self.maptype][self.output][1][1]
         if isinstance(self.output, int):
             self.question = self.questions[self.maptype][self.output][0]
         
     def noButton(self):
         self.isChange = True
-        self.output = self.questions[self.maptype][self.output][1][0]
+        if isinstance(self.output, int):
+            self.output = self.questions[self.maptype][self.output][1][0]
         if isinstance(self.output, int):
             self.question = self.questions[self.maptype][self.output][0]
 
-    def updateQuestion(self, quest, inf, mapt):
-        self.isChange = True
-        self.question = quest
-        self.info = inf
-        self.maptype = mapt
+    def parseInfo(self):
+        self.endString = 'Flagged Questions:\n'
+        for i in self.flags:
+            self.endString = self.endString + i[0] + ' : ' + i[1] + '\n'
+        self.endString = self.endString + '\nCritical Questions:\n'
+        for i in self.critical:
+            self.endString = self.endString + i[0] + ' : ' + i[1] + '\n'
+        self.testName = "Results/" + self.testName.replace(' ', '_') + '.txt'
 
-    def addCritical(self):
-        if self.output == 'Critical':
-            self.critical.append(self.critTrack)
+    def endBox(self):
+        self.root.destroy()
+        self.end = ThemedTk(theme='arc').title('End Screen')
+        self.parseInfo()
+        self.TextBox = ttk.Label(self.end, text=self.endString, font=('Times New Roman', 16)).pack()
+        self.file = open(self.testName, 'x')
+        self.file.write(self.endString)
